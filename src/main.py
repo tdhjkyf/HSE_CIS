4e4061096be8222be57c27c7b8132163617a11d6
Enders Dmitrii <dima.enders@icloud.com>
2026-03-02 18:04:39 +0100

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+def main():
+    print("Hello from src/main.py!")
+
+    a = 7
+    b = 3
+    print(f"{a} + {b} = {a + b}")
+
+    if (a + b) % 2 == 0:
+        print("Сумма чётная")
+    else:
+        print("Сумма нечётная")
+
+
+if __name__ == "__main__":
+    main()
